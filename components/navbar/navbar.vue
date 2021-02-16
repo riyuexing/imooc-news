7e6b9a3fbb04fa20d3a7cf3667104c5283d0868c
alexander360 <1401527242@qq.com>
2021-02-16 21:59:25 +0800

    5.3 自定义导航栏

---
+++ components/navbar/navbar.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="navbar">
+		<view class="navbar-search">
+			<view class="navbar-search_icon"></view>
+			<view class="navbar-search_text">uni-app、vue</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.navbar {
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 99;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 0 15px;
+		width: 100%;
+		height: 45px;
+		background-color: $mk-base-color;
+		box-sizing: border-box;
+
+		.navbar-search {
+			display: flex;
+			align-items: center;
+			padding: 0 10px;
+			width: 100%;
+			height: 30px;
+			border-radius: 15px;
+			background-color: #FFFFFF;
+
+			.navbar-search_icon {
+				width: 10px;
+				height: 10px;
+				border: 1px red solid;
+				margin-right: 10px;
+			}
+
+			.navbar-search_text {
+				font-size: 12px;
+				color: #999999;
+			}
+		}
+	}
+</style>
--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,9 +1,7 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<!-- 自定义组件 -->
+		<navbar></navbar>
 	</view>
 </template>
 
@@ -11,7 +9,6 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
 			}
 		},
 		onLoad() {
@@ -24,29 +21,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
 </style>
